d2aff9f9259932e81c2f268b19b5285ba72c7dbe
i1hwan <mtslzx@gmail.com>
2022-08-06 00:23:05 +0900

    modified:   "print(\"Hello world\").py"

--- print("Hello world").py
+++ print("Hello world").py
@@ -1,1 +1,9 @@
-print("Hello world")
+option = input("Press Enter to continue...")
+if option == "Windows":
+    print("Hello Windows")
+elif option == "Linux":
+    print("Hello Linux")
+elif option == "Mac":
+    print("Hello Mac")
+else:
+    print("Hello World")
