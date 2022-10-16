ddd40727999eb2b62ac4b25d3d9e598888a85426
i1hwan <mtslzx@gmail.com>
2022-10-16 21:45:44 +0900

    modified:   CS/test.py

--- CS/test.py
+++ CS/test.py
@@ -1,5 +1,3 @@
-age = 40
-if age <= 30 and age >= 50:
-    print('30살어쩌고')
-else:
-    print('50살 어쩌고')
+# 1줄 코딩
+score = int(input("성적을 입력하시오: "));print('A' if score >= 90 else 'B' if score >= 80 else 'C' if score >= 70 else 'D' if score >= 60 else 'F')
+number = int(input("정수를 입력하시오: "));print("2와 3으로 나누어 떨어집니다." if number % 2 == 0 and number % 3 == 0 else "2로 나누어 떨어집니다." if number % 2 == 0 else "3으로 나누어 떨어집니다." if number % 3 == 0 else "2와 3으로 나누어 떨어지지 않습니다.")
