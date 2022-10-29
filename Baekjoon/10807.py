9d884cd06dfa373496eb257c9b80c68c87e272dd
i1hwan <mtslzx@gmail.com>
2022-10-29 23:31:21 +0900

    new file:   Baekjoon/10807.py 	new file:   Baekjoon/10818.py

---
+++ Baekjoon/10807.py
@@ -0,0 +1,9 @@
+# Count num
+# So easy in python
+
+# count = int(input())
+# if count == 0:
+#     quit()
+input()
+arr = list(map(int, input().split()))
+print(arr.count(int(input())))
---
+++ Baekjoon/10818.py
@@ -0,0 +1,10 @@
+# Max Min
+input()
+arr = list(map(int, input().split()))
+print(f"{min(arr)} {max(arr)}")
+
+
+# Reference
+# https://rebro.kr/126
+# Github Copilot: https://github.com/
+# Python is so easy
